b5f940ea9f4eb52a35af897c88e0d55cc9270da3
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-01-23 18:24:43 -0500

    add config testing refactorizations

--- test/integration/config/config_service/default_test.rb
+++ test/integration/config/config_service/default_test.rb
@@ -1,4 +1,4 @@
-title "Default role integrated test file"
+title "Consul configuration test suite"
 
 describe directory('/etc/consul/') do
   it { should exist }
@@ -13,4 +13,32 @@ describe file('/etc/consul/test-example.json') do
 
   its('content') { should match('datacenter') }
   its('content') { should match('data_dir') }
+end
+
+describe directory('/mnt/etc/consul') do
+  it { should exist }
+  its('owner') { should eq 'consul' }
+  its('group') { should eq 'consul' }
+end
+
+describe file('/mnt/etc/consul/path-test.json') do
+  it { should exist }
+  its('owner') { should eq 'consul' }
+  its('group') { should eq 'consul' }
+
+  its('content') { should match('datacenter') }
+  its('content') { should match('log_level') }
+  its('content') { should match('log_file') }
+end
+
+describe directory('/var/data/consul') do
+  it { should exist }
+  its('owner') { should eq 'consul' }
+  its('group') { should eq 'consul' }
+end
+
+describe file('/mnt/var/log/consul/consul.log') do
+  it { should exist }
+  its('owner') { should eq 'consul' }
+  its('group') { should eq 'consul' }
 end
